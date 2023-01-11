cf08d94e7c30a07237e2d642cb8bf5d471dea410
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-12 00:55:24 +0200

    added promise coverage test cases

---
+++ promise_test.go
@@ -0,0 +1,216 @@
+package gromise
+
+import (
+	"errors"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestPromiseCreate(t *testing.T) {
+	p := New(func(a ...any) any {
+		return true
+	})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	p.Run(&wg)
+	wg.Wait()
+	if p == nil {
+		t.Fatal("Promise should not be nil")
+	}
+	if p.Error != nil {
+		t.Fatal("Promise should not have an error")
+	}
+	if p.Result == nil {
+		t.Fatal("Promise should have a result")
+	}
+	if *p.Result != true {
+		t.Fatal("Promise result should be true")
+	}
+}
+
+func TestPromiseCreateAndReject(t *testing.T) {
+	p := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	p.Run(&wg)
+	wg.Wait()
+	if p == nil {
+		t.Fatal("Promise should not be nil")
+	}
+	if p.Error == nil {
+		t.Fatal("Promise should have an error")
+	}
+}
+
+func TestPromiseAll(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		return "hello"
+	})
+	p2 := New(func(a ...any) any {
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	results, err := All(promises)
+	if err != nil {
+		t.Fatal("Promise should not have an error")
+	}
+	if len(results) != 2 {
+		t.Fatal("Promise should have 2 results")
+	}
+	if results[0] != "hello" {
+		t.Fatal("Promise result should be hello")
+	}
+	if results[1] != "world" {
+		t.Fatal("Promise result should be world")
+	}
+}
+
+func TestPromiseAllReject(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		return "hello"
+	})
+	p2 := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	promises := []*Promise{p1, p2}
+	results, err := All(promises)
+	if err == nil {
+		t.Fatal("Promise should have an error")
+	}
+	if results != nil {
+		t.Fatal("Promise results should be nil")
+	}
+}
+
+func TestPromiseAllSettled(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		return "hello"
+	})
+	p2 := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	p3 := New(func(a ...any) any {
+		return nil
+	})
+	promises := []*Promise{p1, p2, p3}
+	results := AllSettled(promises)
+	if len(results) != 3 {
+		t.Fatal("Promise should have 3 results")
+	}
+	if results[0] != "fullfilled" {
+		t.Fatal("Promise result should be fullfilled")
+	}
+	if results[1] == "rejected" {
+		t.Fatal("Promise result should be rejected")
+	}
+	if results[2] != "fullfilled" {
+		t.Fatal("Promise result of nil should be fullfilled")
+	}
+}
+
+func TestPromiseAny(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	p2 := New(func(a ...any) any {
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	result, err := Any(promises)
+	if err != nil {
+		t.Fatal("Promise should not have an error")
+	}
+	if result != "world" {
+		t.Fatal("Promise result should be world")
+	}
+}
+
+func TestPromiseAnyReject(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	p2 := New(func(a ...any) any {
+		panic(errors.New("Promise rejected"))
+	})
+	promises := []*Promise{p1, p2}
+	_, err := Any(promises)
+	if err == nil {
+		t.Fatal("Promise should have an error")
+	}
+}
+
+func TestPromiseAnyAllResolved(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		time.Sleep(100 * time.Millisecond)
+		return "hello"
+	})
+	p2 := New(func(a ...any) any {
+		time.Sleep(50 * time.Millisecond)
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	result, err := Race(promises)
+	if err != nil {
+		t.Fatal("Promise should not have an error")
+	}
+	if result != "world" {
+		t.Fatal("Promise result should be world")
+	}
+}
+
+func TestPromiseRace(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		time.Sleep(100 * time.Millisecond)
+		return "hello"
+	})
+	p2 := New(func(a ...any) any {
+		time.Sleep(50 * time.Millisecond)
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	result, err := Race(promises)
+	if err != nil {
+		t.Fatal("Promise should not have an error")
+	}
+	if result != "world" {
+		t.Fatal("Promise result should be world")
+	}
+}
+
+func TestPromiseRaceRejectFirst(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		time.Sleep(50 * time.Millisecond)
+		panic("Promise rejected")
+	})
+	p2 := New(func(a ...any) any {
+		time.Sleep(150 * time.Millisecond)
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	_, err := Race(promises)
+	if err == nil {
+		t.Fatal("Promise should have rejected")
+	}
+}
+
+func TestPromiseRaceRejectAfterFullfill(t *testing.T) {
+	p1 := New(func(a ...any) any {
+		time.Sleep(150 * time.Millisecond)
+		panic("Promise rejected")
+	})
+	p2 := New(func(a ...any) any {
+		time.Sleep(50 * time.Millisecond)
+		return "world"
+	})
+	promises := []*Promise{p1, p2}
+	result, err := Race(promises)
+	if err != nil {
+		t.Fatal("Promise should have been fullfilled")
+	}
+	if result != "world" {
+		t.Fatal("Promise result should be world")
+	}
+}
