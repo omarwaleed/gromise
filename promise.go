1c5eae297ac63b21d1afe15ea667f0c7be8b1a60
omarwaleed <omarpersonalprofile@gmail.com>
2023-01-12 01:03:02 +0200

    added functionlity for most missing executable functions

--- promise.go
+++ promise.go
@@ -1,6 +1,9 @@
 package gromise
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
 
 type Promise struct {
 	Fn     (func(...any) any)
@@ -8,6 +11,12 @@ type Promise struct {
 	Error  error
 }
 
+type promiseArrayWithMutex struct {
+	promises []*Promise
+	mu       sync.Mutex
+}
+
+// Creates a new promise. Returns a value if resolved or rejects if function panics. Does NOT run the promise.
 func New(fn func(...any) any) *Promise {
 	p := &Promise{
 		Fn: fn,
@@ -15,46 +24,93 @@ func New(fn func(...any) any) *Promise {
 	return p
 }
 
+// Runs the promise given
 func (p *Promise) Run(wg *sync.WaitGroup) {
 	go func() {
 		defer func() {
 			if err := recover(); err != nil {
 				p.Error = err.(error)
 			}
-			wg.Done()
+			if wg != nil {
+				wg.Done()
+			}
 		}()
 		result := p.Fn()
 		p.Result = &result
 	}()
 }
 
+// Given a list of promises, runs them all and returns the results. Rejects if any promise panics.
 func All(promises []*Promise) ([]any, error) {
 	results := []any{}
 	wg := sync.WaitGroup{}
 	wg.Add(len(promises))
 	for _, promise := range promises {
 		promise.Run(&wg)
+	}
+	wg.Wait()
+	for _, promise := range promises {
 		if promise.Error != nil {
 			return nil, promise.Error
 		}
 		results = append(results, *promise.Result)
 	}
-	wg.Wait()
 	return results, nil
 }
 
+// Given a list of promises, runs them all and returns the status of each promise fullfilled/rejected.
 func AllSettled(promises []*Promise) []any {
 	results := make([]any, len(promises))
 	wg := sync.WaitGroup{}
+	wg.Add(len(promises))
 	for _, promise := range promises {
-		wg.Add(1)
 		promise.Run(&wg)
-		if promise.Error != nil {
-			results = append(results, promise.Error)
-		} else {
-			results = append(results, *promise.Result)
-		}
 	}
 	wg.Wait()
+	for _, promise := range promises {
+		if promise.Error != nil {
+			results = append(results, "rejected")
+		} else {
+			results = append(results, "fulfilled")
+		}
+	}
 	return results
+}
+
+// Given a list of promises, runs them all and returns the first non rejected result. Rejects if all promises reject.
+func Any(promises []*Promise) (any, error) {
+	wg := sync.WaitGroup{}
+	wg.Add(len(promises))
+	for _, promise := range promises {
+		promise.Run(&wg)
+	}
+	wg.Wait()
+	for _, promise := range promises {
+		if promise.Error == nil {
+			return promise.Result, nil
+		}
+	}
+	return nil, errors.New("All promises rejected")
+}
+
+// Given a list of promises, runs them all and returns the first non rejected result. Rejects if all promises reject.
+func Race(promises []*Promise) (any, error) {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	pa := promiseArrayWithMutex{
+		mu:       sync.Mutex{},
+		promises: promises,
+	}
+	for _, promise := range promises {
+		promise.Run(&wg)
+	}
+	wg.Wait()
+	pa.mu.Lock()
+	defer pa.mu.Unlock()
+	for _, promise := range pa.promises {
+		if promise.Error == nil {
+			return promise.Result, nil
+		}
+	}
+	return nil, errors.New("All promises rejected")
 }
